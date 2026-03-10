ee059b7f4bee0be11337eba08fa5f855c6a7f5ff
esdrfgbv <subrahmanyamkolipakula@gmail.com>
2026-03-10 17:05:36 +0530

    First commit

---
+++ src/main/java/com/example/sanchari/SanchariApplication.java
@@ -0,0 +1,13 @@
+package com.example.sanchari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanchariApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanchariApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/sanchari/auth/authservice.java
@@ -0,0 +1,7 @@
+package com.example.sanchari.auth;
+
+public class authservice {
+    public String hello() {
+        return "Hello World";
+    }
+}
